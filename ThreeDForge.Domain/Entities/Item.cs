122bd86d4a96aa10998cd5628a1632e6cdaa5e1a
Stan Gilstrap <Stangil44@Gmail.com>
2024-10-16 16:22:56 -0400

    Add project files.

---
+++ ThreeDForge.Domain/Entities/Item.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using ThreeDForge.Domain.Enums;
+
+namespace ThreeDForge.Domain.Entities
+{
+    public class Item
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public DateTime AddedDate { get; set; }
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ ThreeDForge.Domain/Enums/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeDForge.Domain.Enums
+{
+    public enum Category
+    {
+        Misc,Figure,Vase,Sticker
+    }
+}
